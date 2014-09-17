7f40112717ff8c4626ae629b21063c002e292930
Matthew Walker <mattwalker101@hotmail.com>
2014-09-17 09:57:34 -0700

    Update Ex4_20.java

--- 4_20/Ex4_20.java
+++ 4_20/Ex4_20.java
@@ -15,26 +15,42 @@
 
 package ex4_20;
 
+import java.util.Scanner;
+
 public class Ex4_20 {
 
     public static void main(String[] args) {
-    
-        Salary salary1 = new Salary("", 0.0, 0.0);
-        Salary salary2 = new Salary("", 0.0, 0.0);
-        Salary salary3 = new Salary("", 0.0, 0.0);
-     
+         
         System.out.println( "Welcome to Matthew's Amazing Salary Calculator!");
         System.out.println( "You will input data for 3 employees, and I will tell you their weekly wages");
         System.out.println( "***********************************************");
-    
-        // gather user input
-        salary1.setEmployeeData();
-        salary2.setEmployeeData();
-        salary3.setEmployeeData();
         
-        // Calculate & display pay for each employee
-        salary1.displaySalary();
-        salary2.displaySalary();
-        salary3.displaySalary();
+        int counter = 0;
+        String name;
+        double hours, rate;
+        
+        // gather user input
+        Scanner input = new Scanner( System.in );
+
+        while ( counter < 3 ) {
+            counter++;
+            System.out.printf( "Input employee name: ");
+            name = input.next();
+            System.out.printf( "Their hours worked this week: ");
+            hours = input.nextDouble();
+            System.out.printf( "Their hourly wage is: ");
+            rate = input.nextDouble();
+        
+            // Calculate & display pay for each employee
+            if ( hours <= 40 ) {
+                System.out.printf( "Employee %s earned $%.2f this week.\n\n", name, 
+                    ( hours*rate ) ); 
+            } else {
+                System.out.printf( "Employee %s earned $%.2f this week, with overtime.\n\n",
+                    name, ( (40*rate) + ( (hours-40 )*rate*1.5 ) ) );
+            }
+                 
+        }
     }
 }
+
