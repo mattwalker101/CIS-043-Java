bc2d3d1cd7c40a140bee26ab74638f121a2e4e6f
Matthew Walker <mattwalker101@hotmail.com>
2014-09-24 09:38:03 -0700

    Update Ex5_12.java

--- 5_12-For-Loop/Ex5_12.java
+++ 5_12-For-Loop/Ex5_12.java
@@ -19,6 +19,22 @@ public class Ex5_12 {
 
     public static void main(String[] args) {
         
+        System.out.printf( "Welcome! This is the product of odd #s 1-15!\n");
+        
+        // initialize loop and print
+        int product = 1;
+        
+        for ( int i = 1; i <=15; i+=2 ) {
+            product *= i;
+        }
+        
+        // display output
+        System.out.printf("Product is %d!\n", product);
+        
+ 
+        // I first read the problem to be make a table of the products of odd
+        // integers between 1-15. Here is the code I wrote for that interpretation
+        /*
         System.out.printf( "Welcome! Here are some products of odd integers!\n" );
         System.out.printf( "*****************************************\n");
         System.out.printf( "(#1)  x  (#2)\t(PRODUCT)\n");
@@ -29,5 +45,8 @@ public class Ex5_12 {
                 System.out.printf("%3d   x  %3d   = %5d\n", i, j, i*j);
             }
         }
+                */
+        
     }
 }
+ 
