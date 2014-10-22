2aea3f3c3dc4c0df80f7bb830b5edc139f3af3eb
Matthew Walker <mattwalker101@hotmail.com>
2014-10-22 09:36:48 -0700

    Update Ex8_4.java

--- 8_4/Ex8_4.java
+++ 8_4/Ex8_4.java
@@ -1,2 +1,19 @@
 /*
+Rectangle
+-------
+- length: double
+- Width: double
+-------
+Rectangle()   // no argument constructor
+Rectangle(l:double, w:double)   // full argument constructor
+Rectangle(l:double)   // optional single argument constructor
++ getLength(): double
++ setLength(l:double): void
++ getWidth(): double
++ setWidth(w:double): void
+
++ getArea(): double
++ getPerimeter(): double
++ toString(): String // should contain this rectangle's length, width, area, perimeter info
+
 */
