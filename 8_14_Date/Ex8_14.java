91db3c1a3e0882459c7bb499e3bc93ffde0d14c0
Matthew Walker <mattwalker101@hotmail.com>
2014-10-29 11:19:10 -0700

    Update Ex8_14.java

--- 8_14_Date/Ex8_14.java
+++ 8_14_Date/Ex8_14.java
@@ -15,19 +15,60 @@
 
 package ex8_14;
 
+import java.util.Scanner;
+
 public class Ex8_14 {
 
     public static void main(String[] args) {
 
-    Date d1 = new Date(10, 22, 2014);  
-    Date d2 = new Date("January", 15, 2014);
-    Date d3 = new Date(200, 2014);
+        Date d1 = new Date();  
+        Scanner input = new Scanner( System.in );
+        int choice;
     
-    
-    
-    System.out.printf("%s", d1.toString() ); // prints 10/22/2014
-                                            // also print October 14, 2014
-                                            // and 295 2014
+        System.out.println("This is a super Date Converter!\n");
+        d1.displayMenu();
+        choice = input.nextInt();
+        while (choice != 4 ) {
+            switch(choice){
+                case 1:
+                    System.out.print("Enter Month(1-12): ");
+                    d1.setMonth(input.nextInt());
+                    System.out.print("Enter Day of Month: ");
+                    d1.setDay(input.nextInt());
+                    System.out.print("Enter Year: ");
+                    d1.setYear(input.nextInt());
+                    
+                    System.out.printf("\n%s", d1.toAllString());
+                    System.out.println("---------------\n");
+                    d1.displayMenu();
+                    choice = input.nextInt();
+                    break;
+                case 2:
+                    System.out.print("Enter Month Name: ");
+                    d1.setMonth(input.next());
+                    System.out.print("Enter Day of Month: ");
+                    d1.setDay(input.nextInt());
+                    System.out.print("Enter Year: ");
+                    d1.setYear(input.nextInt());
+                    
+                    System.out.printf("\n%s", d1.toAllString());
+                    System.out.println("---------------\n");
+                    d1.displayMenu();
+                    choice = input.nextInt();
+                    break;
+                case 3:
+                    System.out.print("Enter Day of Year: ");
+                    d1.setDay(input.nextInt());
+                    System.out.print("Enter Year: ");
+                    d1.setYear(input.nextInt());
+                    
+                    System.out.printf("\n%s", d1.toAllString());
+                    System.out.println("---------------\n");
+                    d1.displayMenu();
+                    choice = input.nextInt();
+                    break;
+                    
+            }
+        }
     }
-    
 }
