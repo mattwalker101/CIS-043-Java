123da82696b382269805307623f8e9388e3e2a49
Matthew Walker <mattwalker101@hotmail.com>
2014-11-10 09:41:46 -0800

    Create Ex11_18.java

---
+++ 11_18_Exception_Handling/Ex11/Ex11_18.java
@@ -0,0 +1,16 @@
+/*********************************************************
+ *                                                        
+ *    Exercise: 11.18                                          
+ *                                                        
+ *    Purpose:  Handling exceptions                                          
+ *                                                        
+ *    Author: Matthew Walker                              
+ *                                                        
+ *    Class: CIS 043 - Software Development with Java     
+ *           Mission College, Santa Clara, CA             
+ *                                                        
+ *    Due Date: 11/10/14                                              
+ *                                                        
+ **********************************************************/
+ 
+ 
