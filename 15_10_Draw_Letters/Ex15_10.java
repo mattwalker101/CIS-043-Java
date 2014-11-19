01524004119f0ea205ba8beb20c78f7cc51c68be
Matthew Walker <mattwalker101@hotmail.com>
2014-11-19 10:10:46 -0800

    Update Ex15_10.java

--- 15_10_Draw_Letters/Ex15_10.java
+++ 15_10_Draw_Letters/Ex15_10.java
@@ -1,5 +1,20 @@
 package ex15_10;
 
+/*********************************************************
+ *                                                        
+ *    Exercise: 15.10                                          
+ *                                                        
+ *    Purpose: Draw random letters in a frame                                           
+ *                                                        
+ *    Author: Matthew Walker                              
+ *                                                        
+ *    Class: CIS 043 - Software Development with Java     
+ *           Mission College, Santa Clara, CA             
+ *                                                        
+ *    Due Date: 11/24/14                                              
+ *                                                        
+ **********************************************************/
+ 
 import javax.swing.JFrame;
 
 public class Ex15_10 {
