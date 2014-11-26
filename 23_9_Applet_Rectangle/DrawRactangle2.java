afa3b5a58e3ce1d2993a6ada86fc890e8923edff
Matthew Walker <mattwalker101@hotmail.com>
2014-11-26 09:43:43 -0800

    Update DrawRactangle2.java

--- 23_9_Applet_Rectangle/DrawRactangle2.java
+++ 23_9_Applet_Rectangle/DrawRactangle2.java
@@ -1,3 +1,19 @@
+/*********************************************************
+ *                                                        
+ *    Exercise: 23.9                                          
+ *                                                        
+ *    Purpose: Use applet to draw user-specified rectangle                                           
+ *                                                        
+ *    Author: Matthew Walker                              
+ *                                                        
+ *    Class: CIS 043 - Software Development with Java     
+ *           Mission College, Santa Clara, CA             
+ *                                                        
+ *    Due Date: 12/1/14                                               
+ *                                                        
+ **********************************************************/
+package ex23_9;
+
 // Exercise 23.9 Solution: DrawRectangle2.java
 // Draws a rectangle on the applet window whose
 // dimension and location are specified by the user
@@ -5,7 +21,7 @@ import java.awt.Graphics;
 import javax.swing.JApplet;
 import javax.swing.JOptionPane;
 
-public class DrawRectangle2 extends JApplet 
+public class DrawRectangle extends JApplet 
 {
    int upperLeftX; 
    int upperLeftY; 
@@ -23,13 +39,16 @@ public class DrawRectangle2 extends JApplet
       upperLeftX = Integer.parseInt( inputString );
 
       // read upper right y-value from user
-     
+     inputString = JOptionPane.showInputDialog( "Enter upper left y :" );
+     upperLeftY = Integer.parseInt( inputString );
 
       // read width from user
-      
+     inputString = JOptionPane.showInputDialog( "Enter width :" );
+     width = Integer.parseInt( inputString ); 
 	  
       // read height from user
-      
+     inputString = JOptionPane.showInputDialog( "Enter height :" );
+     height = Integer.parseInt( inputString ); 
 	  
    } // end method init
 
@@ -37,8 +56,9 @@ public class DrawRectangle2 extends JApplet
    public void paint( Graphics g )
    {
       // draw user-specified rectangle 
+       g.drawRect( upperLeftX, upperLeftY, width, height );
 	  
    } 
-} // end class DrawRectangle2
+} // end class DrawRectangle
 
-/
+
