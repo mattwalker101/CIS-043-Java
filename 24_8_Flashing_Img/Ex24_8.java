6b55716daa6b49f8376b8c3ebb95f206f7dbd6f6
Matthew Walker <mattwalker101@hotmail.com>
2014-11-26 10:17:58 -0800

    Update Ex24_8.java

--- 24_8_Flashing_Img/Ex24_8.java
+++ 24_8_Flashing_Img/Ex24_8.java
@@ -1,9 +1,26 @@
-// Exercise 24.8 Solution: Flash2.java
-// Program flashes an image.
+/*********************************************************
+ *                                                        
+ *    Exercise: 24.8                                         
+ *                                                        
+ *    Purpose: Flash an image                                           
+ *                                                        
+ *    Author: Matthew Walker                              
+ *                                                        
+ *    Class: CIS 043 - Software Development with Java     
+ *           Mission College, Santa Clara, CA             
+ *                                                        
+ *    Due Date: 12/1/14                                               
+ *                                                        
+ **********************************************************/
+
+package ex24_8;
+
 import javax.swing.JFrame;
 
-public class Flash2 
-{
+public class Ex24_8 {
+
+// Exercise 24.8 Solution: Flash2.java
+// Program flashes an image.
    public static void main( String args[] )
    {
       Flash2JPanel flash2JPanel = new Flash2JPanel();
@@ -15,19 +32,3 @@ public class Flash2
       window.setVisible( true );
    } // end main
 } // end class Flash2
-
-/**************************************************************************
- * (C) Copyright 1992-2010 by Deitel & Associates, Inc. and               *
- * Prentice Hall. All Rights Reserved.                                    *
- *                                                                        *
- * DISCLAIMER: The authors and publisher of this book have used their     *
- * best efforts in preparing the book. These efforts include the          *
- * development, research, and testing of the theories and programs        *
- * to determine their effectiveness. The authors and publisher make       *
- * no warranty of any kind, expressed or implied, with regard to these    *
- * programs or to the documentation contained in these books. The authors *
- * and publisher shall not be liable in any event for incidental or       *
- * consequential damages in connection with, or arising out of, the       *
- * furnishing, performance, or use of these programs.                     *
- *************************************************************************/
-
