75a734aa778f5957eec92e2edb770bb2ebfa4ab9
Matthew Walker <mattwalker101@hotmail.com>
2014-12-01 11:38:55 -0800

    Update Compare.java

--- 16_4_String_Compare/Compare.java
+++ 16_4_String_Compare/Compare.java
@@ -1,3 +1,20 @@
+package compare;
+
+/*********************************************************
+ *                                                        
+ *    Exercise: 16.4                                          
+ *                                                        
+ *    Purpose: Compare two strings                                           
+ *                                                        
+ *    Author: Matthew Walker                              
+ *                                                        
+ *    Class: CIS 043 - Software Development with Java     
+ *           Mission College, Santa Clara, CA             
+ *                                                        
+ *    Due Date: 12/8/10                                              
+ *                                                        
+ **********************************************************/
+ 
 // Exercise 16.4 Solution: Compare.java
 // Application compares two Strings.
 import java.util.Scanner;
@@ -6,18 +23,36 @@ public class Compare
 {
    public static void main( String args[] )
    {
-      // get two strings from user
-      
+            String s1 = "";
+            String s2 = "";
+            Scanner input = new Scanner( System.in );
+       
+            // get two strings from user
+            System.out.print("Enter the first string: ");
+            s1 = input.nextLine();
+        
+            System.out.print("\nEnter the second string: ");
+            s2 = input.nextLine();
+                
+            // get number of character to compare and starting index
+            System.out.print("\nHow many characters should I compare? ");
+            int l = input.nextInt();
 
-      // get number of character to compare and starting index
-      
+            System.out.print("Enter starting index for comparison: ");
+            int i = input.nextInt();
 
-      // compare strings using regionMatches
-      
-
-      // display result
-     
-	 
-   } // end main
+            // compare strings using regionMatches
+            boolean isMatch = false;
+            if ( s1.regionMatches(0, s2, i, l))
+                isMatch = true;
+        
+            // display result
+            if (isMatch)
+                System.out.printf("Match of length [%d] found at index [%d]!\n", l, i);
+            else 
+                System.out.printf("No match of length [%d] found at index [%d]!", l, i);
+            
+        System.out.println("\nGoodbye!\n\n");
+    } // end main
 } // end class Compare
 
